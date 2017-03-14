6861119440d494374806c4c6e971fbbdfcb8bfbd
Matt Bettinson <mattbettinson@me.com>
2017-03-14 16:34:05 -0400

    Messed up a line

--- db/migrate/20170310173513_create_bookmarks_tags.rb
+++ db/migrate/20170310173513_create_bookmarks_tags.rb
@@ -2,7 +2,7 @@ class CreateBookmarksTags < ActiveRecord::Migration[5.0]
   def change
     create_table :bookmarks_tags, id: false do |t|
       t.belongs_to :bookmark, index: true
-      t.belongs_to :tag, index: trua
+      t.belongs_to :tag, index: true
     end
   end
 end
